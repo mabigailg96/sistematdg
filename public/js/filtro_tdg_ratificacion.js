eb9bf0994648eb91ba32758b3cc4851f066bb0c2
luiscerna <luizzroberth@gmail.com>
2019-09-30 17:59:29 -0600

    Añadiendo llenar el select de escuela de filtrar tdg para ratificaciones

--- public/js/filtro_tdg_ratificacion.js
+++ public/js/filtro_tdg_ratificacion.js
@@ -26,6 +26,9 @@ $(document).ready(function(){
 
     // Al cargar la página que la tabla esté sin información
     cargarDataTable();
+
+    // Cargar la lista de las escuelas en el select del filtro
+    cargarSelectEscuela();
       
 
     // Cargar datos a la tabla
@@ -164,4 +167,28 @@ function cargarDataTable(){
     table
         .clear()
         .draw();
+}
+
+// Función para llenar el select con los nombres de escuela
+function cargarSelectEscuela() {
+    // Función de axios para hacer la consulta
+    axios.get('/todos/colleges')
+    .then(response => {
+        //console.log(response);
+
+        // Llenar el select con los elementos traidos
+        response.data.forEach(element => {
+            $("#select-filtro-escuela").append(new Option(element.escuela, element.id));  
+        });
+    }).catch(e => {
+        // Imprimir error en consola
+        console.log(e);
+
+        // Mostrar mensaje de error en caso de que algo haya salido mal con la consulta
+        Swal.fire({
+            type: 'error',
+            title: 'Oops...',
+            text: '¡Algo ha salido mal!, por favor intente más tarde.',
+        });
+    });
 }
