def2b2a15569aa2f3242b6d858d6607efb256dc2
walterluna <mywalter97@gmail.com>
2019-11-07 09:25:32 -0600

    Más cambios en botones regrear

--- resources/views/professor/editar.blade.php
+++ resources/views/professor/editar.blade.php
@@ -10,8 +10,7 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">
-                    Modificacion de datos del  Profesor
-                    <a href="{{ route('professor.index') }}" align="right" class="btn btn-primary btn-color offset-5" role="button">Regresar</a>
+                    Modificacion de datos del  docente
                 </div>
                 <div class="card-body">
                         @if (session('status'))
@@ -21,90 +20,83 @@
                     @endif
 
                     <form  class="form-horizontal" action="{{ route('professor.update', $professor->id) }}" method="post" enctype="multipart/form-data">
-                            @csrf
+                        @csrf
+
                         <div class="row form-group{{ $errors->has('nombre') ? ' has-error' : '' }}">
-                                    <label for="nombre" class="textlabel col-md-3 offset-1 control-label required">Nombres: </label>
-                                        <div class="col-md-6">
-                                            <input id="nombre" type="text" class="form-control" name="nombre" value="{{ $professor->nombre }}" required>
-                                            @if ($errors->has('nombre'))
-                                                <span class="help-block">
-                                                {{ $errors->first('nombre') }}
-                                                </span>
-                                            @endif
-                                        </div>
-                             </div>
-                             <div class="row form-group{{ $errors->has('apellido') ? ' has-error' : '' }}">
-                                    <label for="apellido" class="textlabel col-md-3 offset-1 control-label required">Apellidos: </label>
-                                        <div class="col-md-6">
-                                            <input id="apellido" type="text" class="form-control" name="apellido" value="{{ $professor->apellido }}" required>
-                                            @if ($errors->has('apellido'))
-                                                <span class="help-block">
-                                                {{ $errors->first('apellido') }}
-                                                </span>
-                                            @endif
-                                        </div>
-                             </div>
-                             <div class="row form-group{{ $errors->has('codigo') ? ' has-error' : '' }}">
-                                    <label for="codigo" class="textlabel col-md-3 offset-1 control-label required">Codigo: </label>
-                                        <div class="col-md-6">
-                                            <input id="codigo" type="text" class="form-control" name="codigo" value="{{ $professor->codigo }}" required>
-                                            @if ($errors->has('codigo'))
-                                                <span class="help-block">
-                                                {{ $errors->first('codigo') }}
-                                                </span>
-                                            @endif
-                                        </div>
-                             </div>
+                            <label for="nombre" class="textlabel col-md-3 offset-1 control-label required">Nombres: </label>
+                            <div class="col-md-6">
+                                <input id="nombre" type="text" class="form-control" name="nombre" value="{{ $professor->nombre }}" required>
+                                @if ($errors->has('nombre'))
+                                    <span class="help-block">
+                                        {{ $errors->first('nombre') }}
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        
+                        <div class="row form-group{{ $errors->has('apellido') ? ' has-error' : '' }}">
+                            <label for="apellido" class="textlabel col-md-3 offset-1 control-label required">Apellidos: </label>
+                            <div class="col-md-6">
+                                <input id="apellido" type="text" class="form-control" name="apellido" value="{{ $professor->apellido }}" required>
+                                @if ($errors->has('apellido'))
+                                    <span class="help-block">
+                                        {{ $errors->first('apellido') }}
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
 
-                             <div class="row form-group{{ $errors->has('estado') ? ' has-error' : '' }}">
-                                    <label class="textlabel col-md-3 offset-1 control-label required" for="estado">Estado del profesor</label>
-                                    <div class="urlinput col-md-6">
-                                        <select id="estado" class="form-control" name="estado" value="{{ old('estado' )}}"  class="form-control col-8" required>
+                        <div class="row form-group{{ $errors->has('codigo') ? ' has-error' : '' }}">
+                            <label for="codigo" class="textlabel col-md-3 offset-1 control-label required">Codigo: </label>
+                            <div class="col-md-6">
+                                <input id="codigo" type="text" class="form-control" name="codigo" value="{{ $professor->codigo }}" required>
+                                @if ($errors->has('codigo'))
+                                    <span class="help-block">
+                                        {{ $errors->first('codigo') }}
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        
+                        <div class="row form-group{{ $errors->has('estado') ? ' has-error' : '' }}">
+                            <label class="textlabel col-md-3 offset-1 control-label required" for="estado">Estado:</label>
+                            <div class="urlinput col-md-6">
+                                <select id="estado" class="form-control" name="estado" value="{{ old('estado' )}}"  class="form-control col-8" required>
+                                    @if ($professor->estado == 1)
+                                        <option value="{{ $professor->estado }}" selected>Activo</option >
+                                        <option value="0" >Inactivo </option >
+                                    @else
+                                        <option value="{{ $professor->estado }}" selected>Inactivo</option >
+                                        <option value="1" >Activo </option >
+                                    @endif
+                                </select>
+                                @if ($errors->has('escuela_id'))
+                                    <span class="help-block">
+                                        {{ $errors->first('escuela_id') }}
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
 
-
-                                         @if ($professor->estado == 1)
-                                         <option value="{{ $professor->estado }}" selected>Activo</option >
-                                            <option value="0" >Inactivo </option >
-                                         @else
-                                         <option value="{{ $professor->estado }}" selected>Inactivo</option >
-                                            <option value="1" >Activo </option >
-                                         @endif
-
-
-
-                                        </select>
-                                        @if ($errors->has('escuela_id'))
-                                            <span class="help-block">
-                                                {{ $errors->first('escuela_id') }}
-                                            </span>
-                                        @endif
-                                    </div>
-                                </div>
-                                <div class="row form-group">
-                                        <div class="col-md-2 offset-4">
-                                            <button type="submit" class="btn btn-primary btn-color" id="guardar">
-                                                Guardar Profesor
-                                            </button>
-                                        </div>
-                                    </div>
+                        <div class="row form-group">
+                            <div class="col-2 offset-4">
+                                    <a href="{{ route('professor.index') }}" align="right" class="btn btn-danger" role="button">Regresar</a>
+                            </div>
+                            <div class="col-md-2">
+                                <button type="submit" class="btn btn-primary btn-color" id="guardar">
+                                    Guardar Docente
+                                </button>
+                            </div>
+                        </div>
+                        
                     </form>
                     <br>
                 </div>
                 <div class="card-footer text-muted">
-
-                        Todos los campos marcados con <span style="color:red">*</span> son obligatorios y deben ser llenados.
-                        &nbsp;
-                        &nbsp;
-                        &nbsp;
-
-
-
-                    </div>
+                    Todos los campos marcados con <span style="color:red">*</span> son obligatorios y deben ser llenados.
+                </div>
             </div>
-
         </div>
-
     </div>
-
 </div>
 @endsection
--- resources/views/user/ingresar.blade.php
+++ resources/views/user/ingresar.blade.php
@@ -23,103 +23,102 @@
                         @csrf
 
                         <div class="row form-group{{ $errors->has('nombre') ? ' has-error' : '' }}">
-                                <label for="nombre" class="textlabel col-md-3 offset-1 control-label required">Nombre</label>
-                                    <div class="col-md-6">
-                                        <input id="nombre" type="text" class="form-control" name="nombre" value="{{old('nombre')}}" required>
-                                        @if ($errors->has('nombre'))
-                                            <span class="help-block">
-                                            {{ $errors->first('nombre') }}
-                                            </span>
-                                        @endif
-                                    </div>
+                            <label for="nombre" class="textlabel col-md-3 offset-1 control-label required">Nombre</label>
+                            <div class="col-md-6">
+                                <input id="nombre" type="text" class="form-control" name="nombre" value="{{old('nombre')}}" required>
+                                @if ($errors->has('nombre'))
+                                    <span class="help-block">
+                                    {{ $errors->first('nombre') }}
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="row form-group{{ $errors->has('username') ? ' has-error' : '' }}">
+                            <label for="username" class="textlabel col-md-3 offset-1 control-label required">Nombre de usuario</label>
+                            <div class="col-md-6">
+                                <input id="username" type="text" class="form-control" name="username" value="{{old('username')}}" required>
+                                @if ($errors->has('username'))
+                                    <span class="help-block">
+                                    {{ $errors->first('username') }}
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="row form-group{{ $errors->has('escuela_id') ? ' has-error' : '' }}">
+                            <label class="textlabel col-md-3 offset-1 control-label " for="escuela_id">Seleccione la escuela</label>
+                            <div class="urlinput col-md-6">
+                                <select id="escuela_id" class="form-control" name="escuela_id" value=" "  class="form-control col-8">
+                                    <option value="" selected disabled>Seleccione la escuela:</option >
+
+                                </select>
+                                @if ($errors->has('escuela_id'))
+                                    <span class="help-block">
+                                        {{ $errors->first('escuela_id') }}
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="row form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                <label for="email" class="textlabel col-md-3 offset-1 control-label required">Correo Electronico</label>
+                                <div class="col-md-6">
+                                    <input id="email" type="text" class="form-control" name="email" value="{{old('email')}}" required>
+                                    @if ($errors->has('email'))
+                                        <span class="help-block">
+                                        {{ $errors->first('email') }}
+                                        </span>
+                                    @endif
                                 </div>
+                        </div>
 
-                                <div class="row form-group{{ $errors->has('username') ? ' has-error' : '' }}">
-                                        <label for="username" class="textlabel col-md-3 offset-1 control-label required">Nombre de usuario</label>
-                                            <div class="col-md-6">
-                                                <input id="username" type="text" class="form-control" name="username" value="{{old('username')}}" required>
-                                                @if ($errors->has('username'))
-                                                    <span class="help-block">
-                                                    {{ $errors->first('username') }}
-                                                    </span>
-                                                @endif
-                                            </div>
-                                        </div>
+                        <div class="row form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <label for="password" class="textlabel col-md-3 offset-1 control-label required">Contraseña</label>
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control" name="password" value="{{old('password')}}" required>
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                    {{ $errors->first('password') }}
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
 
-                                        <div class="row form-group{{ $errors->has('escuela_id') ? ' has-error' : '' }}">
-                                                <label class="textlabel col-md-3 offset-1 control-label " for="escuela_id">Seleccione la escuela</label>
-                                                <div class="urlinput col-md-6">
-                                                    <select id="escuela_id" class="form-control" name="escuela_id" value=" "  class="form-control col-8">
-                                                        <option value="" selected disabled>Seleccione la escuela:</option >
+                        <div class="row form-group{{ $errors->has('role_id') ? ' has-error' : '' }}">
+                            <label class="textlabel col-md-3 offset-1 control-label required" for="role_id">Seleccione el rol del usuario</label>
+                            <div class="urlinput col-md-6">
+                                <select id="role_id" class="form-control" name="role_id" value=" "  class="form-control col-8" required>
+                                    <option value="" selected disabled>Seleccione el rol del usuario:</option >
+                                        @foreach ($roles as $rol)
+                                        <option value={{$rol->id}}>{{ $rol->name }}</option >
+                                        @endforeach
+                                </select>
+                                @if ($errors->has('escuela_id'))
+                                    <span class="help-block">
+                                        {{ $errors->first('escuela_id') }}
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
 
-                                                    </select>
-                                                    @if ($errors->has('escuela_id'))
-                                                        <span class="help-block">
-                                                            {{ $errors->first('escuela_id') }}
-                                                        </span>
-                                                    @endif
-                                                </div>
-                                            </div>
-                                            <div class="row form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                                                    <label for="email" class="textlabel col-md-3 offset-1 control-label required">Correo Electronico</label>
-                                                        <div class="col-md-6">
-                                                            <input id="email" type="text" class="form-control" name="email" value="{{old('email')}}" required>
-                                                            @if ($errors->has('email'))
-                                                                <span class="help-block">
-                                                                {{ $errors->first('email') }}
-                                                                </span>
-                                                            @endif
-                                                        </div>
-                                                    </div>
-                                                    <div class="row form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                                                            <label for="password" class="textlabel col-md-3 offset-1 control-label required">Contraseña</label>
-                                                                <div class="col-md-6">
-                                                                    <input id="password" type="password" class="form-control" name="password" value="{{old('password')}}" required>
-                                                                    @if ($errors->has('password'))
-                                                                        <span class="help-block">
-                                                                        {{ $errors->first('password') }}
-                                                                        </span>
-                                                                    @endif
-                                                                </div>
-                                                            </div>
-                                                            <div class="row form-group{{ $errors->has('role_id') ? ' has-error' : '' }}">
-                                                                    <label class="textlabel col-md-3 offset-1 control-label required" for="role_id">Seleccione el rol del usuario</label>
-                                                                    <div class="urlinput col-md-6">
-                                                                        <select id="role_id" class="form-control" name="role_id" value=" "  class="form-control col-8" required>
-                                                                            <option value="" selected disabled>Seleccione el rol del usuario:</option >
-                                                                                @foreach ($roles as $rol)
-                                                                                <option value={{$rol->id}}>{{ $rol->name }}</option >
-                                                                                @endforeach
-
-                                                                        </select>
-                                                                        @if ($errors->has('escuela_id'))
-                                                                            <span class="help-block">
-                                                                                {{ $errors->first('escuela_id') }}
-                                                                            </span>
-                                                                        @endif
-                                                                    </div>
-                                                                </div>
-                                                                <div class="row form-group">
-                                                                        <div class="col-md-2 offset-4">
-                                                                            <button type="submit" class="btn btn-primary btn-color">
-                                                                                Guardar usuario
-                                                                            </button>
-                                                                        </div>
-                                                                    </div>
-
-
+                        <div class="row form-group">
+                            <div class="col-2 offset-4">
+                                <a href="{{ route('user.index') }}"  class=" btn btn-danger" role="button">Regresar</a>
+                            </div>
+                            <div class="col-md-2 ">
+                                <button type="submit" class="btn btn-primary btn-color">
+                                    Guardar usuario
+                                </button>
+                            </div>
+                        </div>
 
                     </form>
                     <br>
                   </div>
                   <div class="card-footer text-muted">
                         Todos los campos marcados con <span style="color:red">*</span> son obligatorios y deben ser llenados.
-                        &nbsp;
-                        &nbsp;
-                        &nbsp;
-
-                        <a href="{{ route('user.index') }}"  class=" btn btn-primary btn-color" role="button">Regresar</a>
-                    </div>
+                  </div>
             </div>
 
         </div>
