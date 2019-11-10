12754dd838710953561a0009e6ca65f3ff0a3011
walterluna <mywalter97@gmail.com>
2019-11-10 00:14:55 -0600

    Corrigiendo el mal gusto de pelon

--- resources/views/user/listar_usuarios.blade.php
+++ resources/views/user/listar_usuarios.blade.php
@@ -7,7 +7,7 @@
 @section('content')
 <div class="container">
     <div class=" row justify-content-center">
-        <div class="col-lg-12">
+        <div class="col-lg-10">
             <div class="card">
                 <div  class="card-header">Usuarios del sistema</div>
                     <div class="card-body">
