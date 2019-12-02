7cc37d0876aa74714a8d6acc302ae71a320dc63f
chesterven <44858325+chesterven@users.noreply.github.com>
2019-12-02 11:33:51 -0600

    Title del view para el reporte

--- resources/views/reportes/estadosPdf.blade.php
+++ resources/views/reportes/estadosPdf.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Example 1</title>
+    <title>Reporte TDG FIA</title>
     <link rel="stylesheet" href="css/style.css" media="all" />
   </head>
   <body>
